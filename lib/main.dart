13cc96a0ca220982a7b204deda2db96008920cc1
mitch <mitch@tabian.ca>
2020-06-03 12:51:03 -0700

    clean for new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
-
-import './quiz.dart';
-import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,58 +10,13 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  var _questionIndex = 0;
-  var _totalScore = 0;
-
-  void _resetQuiz(){
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score){
-    _totalScore += score;
-    setState(() {
-      _questionIndex += 1;
-    });
-    print("score: " + _totalScore.toString());
-  }
-
-  final List<Map<String, Object>> _questions = [
-    {
-      QUESTION: "Is Coding In Flow actually my son?",
-      ANSWERS: [
-        {"text": "Yes", "score": 1},
-        {"text": "No", "score": 0}
-        ]
-    },
-    {
-      QUESTION: "Who's your favorite android teacher?",
-      ANSWERS: [
-        {"text": "Coding with Mitch", "score": 1},
-        {"text": "Coding In Flow", "score": 2},
-        {"text": "Donn Felker", "score": 3},
-        {"text": "Nate Ebel", "score": 4},
-      ]
-    },
-    {
-      QUESTION: "Is Flutter for babies?",
-      ANSWERS: [
-        {"text": "Yes", "score": 1},
-        {"text": "No", "score": 0}
-      ],
-    },
-  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("First Flutter App"),),
-        body: _questionIndex < _questions.length
-            ? Quiz(_questions, _questionIndex, _answerQuestion)
-            : Result(_totalScore, _resetQuiz),
+        body: Text("Stuff")
       )
     );
   }
