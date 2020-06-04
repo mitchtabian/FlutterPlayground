14f766ed08ab291b19adf0af69db9cd199af8f7a
mitch <mitch@tabian.ca>
2020-06-04 09:18:51 -0700

    provide static resources for basic detail view

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -18,12 +18,16 @@ class LocationDetail extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           ImageBanner(location.imagePath),
-          TextSection("Summary1", "something 1 hfgh gfh gfhfghfghfg hfg"),
-          TextSection("Summary2", "something 2gf hfgh fghfg hgfh fghfg hfgh fghfghfgh"),
-          TextSection("Summary3 ", "something 3 hgfh fghgf hrtjrtjrtj tjrt j"),
-        ],
+        ]..addAll(textSections(location))
       ),
     );
+  }
+
+  List<Widget> textSections(Location location){
+    return location.facts
+        .map((fact) {
+          return TextSection(fact.title, fact.text);
+    }).toList();
   }
 }
 
