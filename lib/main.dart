d4a155bcf104560c170a9621e606e91c3919d0f2
mitch <mitch@tabian.ca>
2020-06-26 13:37:47 -0700

    reset playground

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:firstflutterapp/providers/great_places_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'screens/places_list_screen.dart';
-import 'screens/add_place_screen.dart';
-import 'screens/place_detail_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,22 +9,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) {
-        return GreatPlacesProvider();
-      },
-      child: MaterialApp(
+    return MaterialApp(
         title: "MyShop",
         theme: ThemeData(
           primarySwatch: Colors.indigo,
           accentColor: Colors.amber,
           fontFamily: "Lato",
         ),
-        home: PlacesListScreen(),
-        routes: {
-          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
-        },
-      ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Title"),
+          ),
+          body: Center(
+            child: Text("some text"),
+          ),
+        ),
     );
   }
 }
