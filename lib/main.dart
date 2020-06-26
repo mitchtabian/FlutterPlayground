e85a5a62c3713305b1ba4bfd1b52e00f1c13c6e1
mitch <mitch@tabian.ca>
2020-06-26 10:45:11 -0700

    start new project for exploring native android features

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'screens/splash_screen.dart';
-import 'providers/auth_provider.dart';
-import 'screens/auth_screen.dart';
-import 'screens/product_detail_screen.dart';
-import 'screens/products_overview_screen.dart';
-import 'providers/products_provider.dart';
-import 'providers/cart_provider.dart';
-import 'providers/order_provider.dart';
-import 'screens/cart_screen.dart';
-import 'screens/edit_product_screen.dart';
-import 'screens/orders_screen.dart';
-import 'screens/user_products_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context){
-            return AuthProvider();
-          },
+    return MaterialApp(
+      title: "MyShop",
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber,
+        fontFamily: "Lato",
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Native Features"),
         ),
-        ChangeNotifierProxyProvider<AuthProvider, ProductsProvider>(
-          create: (BuildContext ctx) {
-            return ProductsProvider();
-          },
-          update: (BuildContext ctx, auth, previous) {
-            previous.setAuthToken(auth.token);
-            previous.setUserId(auth.userId);
-            return previous;
-          },
+        body: Center(
+          child: Text("some stuff"),
         ),
-        ChangeNotifierProvider(
-          create: (context){
-            return CartProvider();
-          },
-        ),
-        ChangeNotifierProxyProvider<AuthProvider, OrderProvider>(
-          create: (BuildContext ctx) {
-            return OrderProvider();
-          },
-          update: (BuildContext ctx, auth, previous) {
-            previous.setAuthToken(auth.token);
-            return previous;
-          },
-        ),
-      ],
-        child: Consumer<AuthProvider>(
-          builder: (ctx, auth, _) => MaterialApp(
-            title: "MyShop",
-            theme: ThemeData(
-              primarySwatch: Colors.purple,
-              accentColor: Colors.deepOrange,
-              fontFamily: "Lato",
-            ),
-            home: auth.isAuth ? ProductsOverviewScreen() : FutureBuilder(
-              future: auth.tryAutoLogin(),
-              builder: (ctx, authResultSnapshot) {
-                return authResultSnapshot.connectionState == ConnectionState.waiting ?
-                SplashScreen() :
-                AuthScreen();
-              },
-            ),
-            routes: {
-              AuthScreen.routeName: (ctx) => AuthScreen(),
-              ProductsOverviewScreen.routeName: (ctx) => ProductsOverviewScreen(),
-              ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-              CartScreen.routeName: (ctx) => CartScreen(),
-              OrdersScreen.routeName: (ctx) => OrdersScreen(),
-              UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-              EditProductScreen.routeName: (ctx) => EditProductScreen(),
-            },
-          )
-        )
+      ),
     );
   }
 }
+
+
 
 
 
